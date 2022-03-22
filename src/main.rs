1b0279860c2c3fca4ed0a27ce7bb8d7583238891
Nick Fagerlund <nick.fagerlund@gmail.com>
2022-03-21 22:31:53 -0700

    Timer..... I don't actually understand what happened here yet.

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ struct Person;
 #[derive(Component, Debug)]
 struct Name(String);
 
+struct GreetTimer(Timer); // ??
+
 // Hmm guess add_startup_system passes it a Commands struct as an argument
 fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Himbo Wilson".to_string()));
@@ -27,10 +29,18 @@ fn add_people(mut commands: Commands) {
 }
 
 // Whoa WH A T, we're using the type signature to query entities' components???
-fn greet_peeps(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        // dbg!(name);
-        println!("low hell, {}!", name.0);
+fn greet_peeps(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<&Name, With<Person>>,
+) {
+    // Update our timer with time elapsed since last tick; if that ends the
+    // timer, squawk.
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            // dbg!(name);
+            println!("low hell, {}!", name.0);
+        }
     }
 }
 
@@ -38,8 +48,13 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         // add app stuff
-        app.add_startup_system(add_people)
-            .add_system(hellow_orld)
+        // per example code:
+            // the reason we call from_seconds with the true flag is to make the timer repeat itself
+        // ...oh. you're just explaining the fact that the second arg to from_seconds is named `repeating`. ok.
+        app
+            .insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
+            // .add_system(hellow_orld)
             .add_system(greet_peeps);
 
     }
