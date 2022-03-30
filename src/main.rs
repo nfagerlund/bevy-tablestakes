177944af7a47dc759c6a23b9192f16cb7b1822af
Nick Fagerlund <nick.fagerlund@gmail.com>
2022-03-29 19:17:28 -0700

    Handle gamepad connection/disconnection
    let's do the actual input in another commit.
    
    and, comment out the hellow spam plugin so I can read the console.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,71 @@ mod hellow;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(hellow::HelloPlugin)
+        // .add_plugin(hellow::HelloPlugin)
         .add_startup_system(setup_sprites)
         .add_system(animate_sprites_system)
+        .add_system(connect_gamepads)
         .run();
 }
+
+// Input time!
+
+struct ActiveGamepad(Gamepad);
+
+fn connect_gamepads(
+    mut commands: Commands,
+    active_gamepad: Option<Res<ActiveGamepad>>,
+    mut gamepad_events: EventReader<GamepadEvent>,
+    // ^^ eventreader params have to be mutable because reading events immutably
+    // still updates an internal tracking cursor on the reader instance. cool.
+) {
+    for GamepadEvent(id, kind) in gamepad_events.iter() {
+        match kind {
+            GamepadEventType::Connected => {
+                info!("pad up: {:?}", id);
+                // let's see, I de-focused the cookbook tab, so what do *I* want to have happen?
+                // First pad in gets it, but if another pad hits start, it'll take over. Nice.
+                if active_gamepad.is_none() {
+                    commands.insert_resource(ActiveGamepad(*id));
+                }
+            },
+            GamepadEventType::Disconnected => {
+                info!("pad down: {:?}", id);
+                // byeeee
+                // ok, I'm back to the example code, what's going on here:
+                if let Some(ActiveGamepad(old_id)) = active_gamepad.as_deref() {
+                    if old_id == id {
+                        commands.remove_resource::<ActiveGamepad>();
+                        // haven't really had to turbofish before now. zoom zoom glub glub.
+                    }
+                }
+            },
+            GamepadEventType::ButtonChanged(GamepadButtonType::Start, val) => {
+                if *val == 1.0 {
+                    info!("Pressed start: {:?}", id);
+                    // If there's an active gamepad...
+                    if let Some(ActiveGamepad(old_id)) = active_gamepad.as_deref() {
+                        // ...but it's not the one you just pressed start on...
+                        if old_id != id {
+                            // ...then let it take over.
+                            commands.insert_resource(ActiveGamepad(*id));
+                            // per the cheatbook: "If you insert a resource of a
+                            // type that already exists, it will be overwritten."
+                        }
+                    }
+                }
+            },
+            // ignore other events for now, ^H^H^H (never mind) but fyi see
+            // examples/input/gamepad_input_events.rs in bevy. The
+            // ButtonChanged(button_type, value) and AxisChanged(axis_type,
+            // value) all return new values as floats, I guess so you can handle
+            // buttons and analog axes with similar code.
+            _ => {},
+        }
+    }
+}
+
+// animation time!
 
 fn animate_sprites_system(
     time: Res<Time>,
