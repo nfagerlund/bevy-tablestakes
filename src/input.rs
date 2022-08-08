bcf2b71de87e4ecb2c976256d225f2a1fa5fdef1
Nick Fagerlund <nick.fagerlund@gmail.com>
2022-08-07 18:29:25 -0700

    GamepadEvent and GamepadAxis are normal structs now, not tuple structs

--- src/input.rs
+++ src/input.rs
@@ -8,8 +8,8 @@ use crate::{
 /// helper function: forward the axes resource (and a gamepad id) to it, get a vec back.
 /// Note: `gilrs`, Bevy's gamepad library, only supports Xinput on windows. boo.
 pub fn get_gamepad_movement_vector(gamepad: Gamepad, axes: Res<Axis<GamepadAxis>>) -> Option<Vec2> {
-    let x_axis = GamepadAxis(gamepad, GamepadAxisType::LeftStickX);
-    let y_axis = GamepadAxis(gamepad, GamepadAxisType::LeftStickY);
+    let x_axis = GamepadAxis::new(gamepad, GamepadAxisType::LeftStickX);
+    let y_axis = GamepadAxis::new(gamepad, GamepadAxisType::LeftStickY);
     let x = axes.get(x_axis)?;
     let y = axes.get(y_axis)?;
     Some(Vec2::new(x, y))
@@ -43,35 +43,35 @@ pub fn connect_gamepads_system(
     // ^^ eventreader params have to be mutable because reading events immutably
     // still updates an internal tracking cursor on the reader instance. cool.
 ) {
-    for GamepadEvent(id, kind) in gamepad_events.iter() {
-        match kind {
+    for GamepadEvent { gamepad, event_type } in gamepad_events.iter() {
+        match event_type {
             GamepadEventType::Connected => {
-                info!("pad up: {:?}", id);
+                info!("pad up: {:?}", gamepad);
                 // let's see, I de-focused the cookbook tab, so what do *I* want to have happen?
                 // First pad in gets it, but if another pad hits start, it'll take over. Nice.
                 if active_gamepad.is_none() {
-                    commands.insert_resource(ActiveGamepad(*id));
+                    commands.insert_resource(ActiveGamepad(*gamepad));
                 }
             },
             GamepadEventType::Disconnected => {
-                info!("pad down: {:?}", id);
+                info!("pad down: {:?}", gamepad);
                 // byeeee
                 // ok, I'm back to the example code, what's going on here:
                 if let Some(ActiveGamepad(old_id)) = active_gamepad.as_deref() {
-                    if old_id == id {
+                    if old_id == gamepad {
                         commands.remove_resource::<ActiveGamepad>();
                         // haven't really had to turbofish before now. zoom zoom glub glub.
                     }
                 }
             },
             GamepadEventType::ButtonChanged(GamepadButtonType::Start, val) if *val == 1.0 => {
-                info!("Pressed start: {:?}", id);
+                info!("Pressed start: {:?}", gamepad);
                 // If there's an active gamepad...
                 if let Some(ActiveGamepad(old_id)) = active_gamepad.as_deref() {
                     // ...but it's not the one you just pressed start on...
-                    if old_id != id {
+                    if old_id != gamepad {
                         // ...then let it take over.
-                        commands.insert_resource(ActiveGamepad(*id));
+                        commands.insert_resource(ActiveGamepad(*gamepad));
                         // per the cheatbook: "If you insert a resource of a
                         // type that already exists, it will be overwritten."
                     }
@@ -86,4 +86,3 @@ pub fn connect_gamepads_system(
         }
     }
 }
-
