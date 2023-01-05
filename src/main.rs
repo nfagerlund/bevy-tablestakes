3dd110c6510489d2b488fd6308b4cd7a1c9bddaa
Nick Fagerlund <nick.fagerlund@gmail.com>
2023-01-04 22:16:48 -0800

    Couple lil events

--- src/main.rs
+++ src/main.rs
@@ -89,6 +89,9 @@ fn main() {
         .add_system_to_stage(CoreStage::PreUpdate, accept_input_system.after(InputSystem))
         // BODY STUFF
         .add_system(shadow_stitcher_system)
+        // MOVEMENT STUFF
+        .add_event::<SolidCollisionEvent>()
+        .add_event::<MovePlanCompletedEvent>()
         // PLAYER STUFF
         .add_startup_system(setup_player)
         .add_system(planned_move_system)
@@ -809,6 +812,13 @@ impl Mobile {
         }
     }
 }
+
+pub struct SolidCollisionEvent {
+    pub mobile: Entity,
+    pub solid: Entity,
+}
+
+pub struct MovePlanCompletedEvent(Entity);
 
 /// Marker component for a spawned LdtkWorldBundle
 #[derive(Component)]
