5d2102599dbbef1bbf5ae4fe91c1843defaa908d
Nick Fagerlund <nick.fagerlund@gmail.com>
2023-10-13 10:05:13 -0700

    Refactor char_animation module into submodules
    I got tired of scrolling through the assetloader stuff to try and find
    my components!

---
+++ src/char_animation/assets.rs
@@ -0,0 +1,323 @@
+//! Asset loader functionality, for juicing aseprite files into a
+//! combination of texture atlases and animation data. This is the messiest
+//! stuff in this module, so it's very nice to have it isolated.
+
+use super::components::*;
+use asefile::AsepriteFile;
+use bevy::asset::{AssetLoader, BoxedFuture, LoadContext, LoadedAsset};
+use bevy::math::{prelude::*, Affine2, Rect};
+use bevy::render::{
+    render_resource::{Extent3d, TextureDimension, TextureFormat},
+    texture::{Image, TextureFormatPixelInfo},
+};
+use bevy::sprite::TextureAtlas;
+use bevy::utils::Duration;
+use image::RgbaImage;
+use std::collections::HashMap;
+
+#[derive(Default)]
+pub struct CharAnimationLoader;
+
+impl AssetLoader for CharAnimationLoader {
+    fn extensions(&self) -> &[&str] {
+        &["aseprite", "ase"]
+    }
+
+    fn load<'a>(
+        &'a self,
+        bytes: &'a [u8],
+        load_context: &'a mut LoadContext,
+    ) -> BoxedFuture<'a, anyhow::Result<()>> {
+        Box::pin(async move {
+            load_aseprite(bytes, load_context)?;
+            Ok(())
+        })
+    }
+}
+
+const REFLECT_Y_COMPONENTS: [f32; 4] = [1.0, 0.0, 0.0, -1.0];
+const REFLECT_Y: Mat2 = Mat2::from_cols_array(&REFLECT_Y_COMPONENTS);
+const OFFSET_TO_CENTER: Vec2 = Vec2::new(-0.5, 0.5);
+
+/// Loads an aseprite file and uses it to construct a sprite sheet `#texture`, a
+/// `#texture_atlas` that indexes into that sprite sheet, and a top-level
+/// `CharAnimation`. The individual `CharAnimationFrames` in the
+/// `CharAnimationVariants` contain indexes into the `TextureAtlas`.
+/// Assumptions:
+/// - File only uses AnimationDirection::Forward.
+/// - Tag names are unique in the file. (Aseprite doesn't guarantee this.)
+/// - Named tags cover all of the needed animation frames.
+///   - OR: there are zero tags and thus only one orientation.
+/// - Walkbox layer: "walkbox"
+/// - Hitbox layer: "hitbox"
+/// - Hurtbox layer: "hurtbox"
+/// - Origin layer: "origin"
+/// - Layers for drawn-on metadata coordinates should be marked as invisible in
+///   the saved file.
+fn load_aseprite(bytes: &[u8], load_context: &mut LoadContext) -> anyhow::Result<()> {
+    let ase = AsepriteFile::read(bytes)?;
+    let width = ase.width();
+    let height = ase.height();
+    let num_frames = ase.num_frames();
+
+    // Build an affine transform for converting the pixel origin point to a
+    // relative anchor position.
+    let texture_size = Vec2::new(width as f32, height as f32);
+    let normalize_scale = Mat2::from_diagonal(texture_size).inverse();
+    let anchor_transform =
+        Affine2::from_mat2_translation(normalize_scale * REFLECT_Y, OFFSET_TO_CENTER);
+
+    // Build the texture atlas, ensuring that its sub-texture indices match the
+    // original Aseprite file's frame indices.
+    let frame_images: Vec<Image> = (0..num_frames)
+        .map(|i| remux_image(ase.frame(i).image()))
+        .collect();
+    // Atlas will be a 1D horizontal strip w/ 1px padding between frames.
+    let atlas_height = height as u32;
+    let atlas_width = width as u32 * num_frames + num_frames - 1;
+    let mut atlas_texture = Image::new_fill(
+        Extent3d {
+            width: atlas_width,
+            height: atlas_height,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        &[0, 0, 0, 0],                 // clear
+        TextureFormat::Rgba8UnormSrgb, // Could frame_images[0].format(), but hardcode for now.
+    );
+    // copy time
+    let mut cur_x = 0_usize;
+    for img in frame_images.iter() {
+        copy_texture_to_atlas(&mut atlas_texture, img, width, height, cur_x, 0);
+        cur_x += width + 1;
+    }
+    // stow texture and get a handle
+    let texture_handle = load_context.set_labeled_asset("texture", LoadedAsset::new(atlas_texture));
+    // atlas time!!!
+    // N.b.: from_grid_with_padding adds grid cells in left-to-right,
+    // top-to-bottom order, and we rely on this to make the frame indices match.
+    let atlas = TextureAtlas::from_grid(
+        texture_handle,
+        Vec2::new(width as f32, height as f32),
+        num_frames as usize,
+        1,
+        Some(Vec2::new(1.0, 0.0)),
+        None,
+    );
+    let atlas_handle = load_context.set_labeled_asset("texture_atlas", LoadedAsset::new(atlas));
+
+    // Since our final frame indices are reliable, processing tags is easy.
+    let mut variants: VariantsMap = HashMap::new();
+
+    // Closure for the heavy lifting (since we can't handle some tags / 0 tags
+    // in the same for-loop):
+    let mut process_frame_range =
+        |name: VariantName, frame_range: core::ops::RangeInclusive<u32>| {
+            let mut total_duration = Duration::default();
+            let frames: Vec<CharAnimationFrame> = frame_range
+                .map(|i| {
+                    let frame = ase.frame(i);
+                    let index = i as usize;
+                    let duration_ms = frame.duration() as u64;
+                    let duration = Duration::from_millis(duration_ms);
+
+                    total_duration += duration;
+
+                    // Wasteful, bc we could exit early on first non-clear px, but meh.
+                    let origin = match rect_from_cel(&ase, "origin", i) {
+                        Some(origin_rect) => origin_rect.min,
+                        None => Vec2::ZERO, // Origin's non-optional.
+                    };
+
+                    // Get the walkbox, offset it relative to the origin, THEN flip the Y.
+                    let absolute_walkbox = rect_from_cel(&ase, "walkbox", i);
+                    let walkbox = absolute_walkbox.map(|r| relative_to_origin(r, origin));
+
+                    // Same deal for hitbox as walkbox.
+                    let absolute_hitbox = rect_from_cel(&ase, "hitbox", i);
+                    let hitbox = absolute_hitbox.map(|r| relative_to_origin(r, origin));
+
+                    let anchor = anchor_transform.transform_point2(origin);
+
+                    CharAnimationFrame {
+                        index,
+                        duration,
+                        origin,
+                        anchor,
+                        walkbox,
+                        hitbox,
+                    }
+                })
+                .collect();
+            let variant = CharAnimationVariant {
+                name,
+                frames,
+                duration: total_duration,
+            };
+            variants.insert(name, variant);
+        };
+
+    if ase.num_tags() == 0 {
+        // then treat whole file as one variant.
+        let frame_range = 0..=(ase.num_frames() - 1);
+        process_frame_range(VariantName::Neutral, frame_range);
+    } else {
+        // one variant per tag.
+        for tag in (0..ase.num_tags()).map(|i| ase.tag(i)) {
+            let name: VariantName = tag.name().try_into()?; // Just propagate error, don't continue load.
+            let frame_range = tag.from_frame()..=tag.to_frame(); // inclusive
+            process_frame_range(name, frame_range);
+        }
+    }
+
+    // Determine directionality... maybe pull this out into a function someday
+    // Anyway, count em up... but, don't bother implementing directionalities I'm not using yet.
+    let directionality = if variants.len() >= 4
+        && variants.contains_key(&VariantName::E)
+        && variants.contains_key(&VariantName::N)
+        && variants.contains_key(&VariantName::W)
+        && variants.contains_key(&VariantName::S)
+    {
+        Directionality::Four
+    } else if variants.contains_key(&VariantName::E) {
+        Directionality::OneE
+    } else {
+        Directionality::Zero
+    };
+
+    // The whole enchilada:
+    let animation = CharAnimation {
+        variants,
+        directionality,
+        texture_atlas: atlas_handle,
+    };
+
+    // And, cut!
+    load_context.set_default_asset(LoadedAsset::new(animation));
+    Ok(())
+}
+
+/// Convert the image buffer returned by `asefile::Frame.image()` into a
+/// `bevy::render::texture::Image`. Consumes the argument and re-uses the
+/// internal container.
+fn remux_image(img: RgbaImage) -> Image {
+    let size = Extent3d {
+        width: img.width(),
+        height: img.height(),
+        depth_or_array_layers: 1,
+    };
+    Image::new(
+        size,
+        TextureDimension::D2,
+        img.into_raw(),
+        TextureFormat::Rgba8UnormSrgb,
+    )
+}
+
+/// Invert the Y coordinates of a Vec2, because source image coordinates go
+/// Y-down but bevy spatial coordinates go Y-up. Note that this is only valid to
+/// do if you're not also moving the origin point!
+fn invert_vec2_y(v: Vec2) -> Vec2 {
+    Vec2::new(v.x, -(v.y))
+}
+
+/// OK, rects have "min" (smallest x, smallest y) and "max" (largest x, largest
+/// y) corners. When you create a rect within a top-down Y coordinate system,
+/// "min" is the top left, and "max" is the bottom right. But if you want to
+/// represent the same rectangle in a bottom-up Y coordinate system, "min" is
+/// the BOTTOM left, and "max" is the TOP right. So it's not just inverting the
+/// Y coordinate of each point, you also have to recombine them. If you don't,
+/// it ends up inside-out.
+fn flip_rect_y(r: Rect) -> Rect {
+    let top_left = r.min;
+    let bottom_right = r.max;
+    let bottom_left = Vec2::new(top_left.x, bottom_right.y);
+    let top_right = Vec2::new(bottom_right.x, top_left.y);
+    Rect {
+        min: invert_vec2_y(bottom_left),
+        max: invert_vec2_y(top_right),
+    }
+}
+
+/// Translate a Rect so its corners are relative to a provided origin/anchor/pivot
+/// point, then flip the corners' Y coordinates.
+fn relative_to_origin(r: Rect, origin: Vec2) -> Rect {
+    let relative = Rect {
+        min: r.min - origin,
+        max: r.max - origin,
+    };
+    flip_rect_y(relative)
+}
+
+/// Get the bounding Rect for a cel's non-transparent pixels.
+fn rect_from_cel(ase: &AsepriteFile, layer_name: &str, frame_index: u32) -> Option<Rect> {
+    ase.layer_by_name(layer_name).and_then(|layer| {
+        let cel_img = layer.frame(frame_index).image();
+        get_rect_lmao(&cel_img)
+    })
+}
+
+/// Get the bounding Rect for the non-transparent pixels in an RgbaImage.
+fn get_rect_lmao(img: &RgbaImage) -> Option<Rect> {
+    let mut x_min: u32 = u32::MAX;
+    let mut x_max: u32 = 0;
+    let mut y_min: u32 = u32::MAX;
+    let mut y_max: u32 = 0;
+    let mut present = false;
+    for (x, y, val) in img.enumerate_pixels() {
+        if non_empty(val) {
+            present = true;
+            if x < x_min {
+                x_min = x;
+            }
+            if x > x_max {
+                x_max = x;
+            }
+            if y < y_min {
+                y_min = y;
+            }
+            if y > y_max {
+                y_max = y;
+            }
+        }
+    }
+    if present {
+        Some(Rect {
+            min: Vec2::new(x_min as f32, y_min as f32),
+            max: Vec2::new(x_max as f32, y_max as f32),
+        })
+    } else {
+        None
+    }
+}
+
+fn alpha(pixel: &image::Rgba<u8>) -> u8 {
+    pixel.0[3]
+}
+
+fn non_empty(pixel: &image::Rgba<u8>) -> bool {
+    alpha(pixel) != 0
+}
+
+// Variation on a TextureAtlasBuilder fn (which I can't use directly bc it
+// relies on runtime asset collections):
+// https://github.com/bevyengine/bevy/blob/c27cc59e0/crates/bevy_sprite/src/texture_atlas_builder.rs#L95
+fn copy_texture_to_atlas(
+    atlas_texture: &mut Image,
+    texture: &Image,
+    rect_width: usize,
+    rect_height: usize,
+    rect_x: usize,
+    rect_y: usize,
+) {
+    let atlas_width = atlas_texture.texture_descriptor.size.width as usize;
+    let format_size = atlas_texture.texture_descriptor.format.pixel_size();
+
+    for (texture_y, bound_y) in (rect_y..rect_y + rect_height).enumerate() {
+        let begin = (bound_y * atlas_width + rect_x) * format_size;
+        let end = begin + rect_width * format_size;
+        let texture_begin = texture_y * rect_width * format_size;
+        let texture_end = texture_begin + rect_width * format_size;
+        atlas_texture.data[begin..end].copy_from_slice(&texture.data[texture_begin..texture_end]);
+    }
+}
---
+++ src/char_animation/components.rs
@@ -0,0 +1,191 @@
+use bevy::asset::Handle;
+use bevy::math::{prelude::*, Rect};
+use bevy::prelude::{Component, Entity, Event};
+use bevy::reflect::Reflect;
+use bevy::reflect::{TypePath, TypeUuid};
+use bevy::sprite::TextureAtlas;
+use bevy::utils::Duration;
+use std::collections::HashMap;
+
+use crate::compass::{self};
+use crate::toolbox::countup_timer::CountupTimer;
+
+#[derive(Debug, TypeUuid, TypePath)]
+#[uuid = "585e2e41-4a97-42ef-a13e-55761c854bb4"]
+pub struct CharAnimation {
+    pub variants: VariantsMap,
+    pub directionality: Directionality,
+    pub texture_atlas: Handle<TextureAtlas>,
+}
+
+#[derive(Debug)]
+pub struct CharAnimationVariant {
+    pub name: VariantName,
+    pub frames: Vec<CharAnimationFrame>,
+    pub duration: Duration,
+}
+
+impl CharAnimationVariant {
+    /// Returns the final frame time to use for the requested frame, after
+    /// applying the specified FrameTimeOverride behavior.
+    pub fn resolved_frame_time(
+        &self,
+        frame_index: usize,
+        override_type: FrameTimeOverride,
+    ) -> Duration {
+        let frame_duration = self.frames[frame_index].duration;
+        match override_type {
+            FrameTimeOverride::None => frame_duration,
+            FrameTimeOverride::Ms(millis) => Duration::from_millis(millis),
+            FrameTimeOverride::Scale(scale) => frame_duration.mul_f32(scale),
+            FrameTimeOverride::TotalMs(total_millis) => {
+                // Split the total milliseconds between the frames, proportional to the fraction of
+                // the total duration they occupy.
+                let millis = total_millis * (frame_duration.as_millis() as u64)
+                    / (self.duration.as_millis() as u64); // Integer division is fine
+                Duration::from_millis(millis)
+            },
+        }
+    }
+}
+
+// I've changed my mind about tags: I'm gonna start out with a rigid enum,
+// enforce tag names at load time, and expand behavior as needed later. Duping
+// the Dir type for easier refactors later??
+pub type VariantName = compass::Dir;
+pub type VariantsMap = HashMap<VariantName, CharAnimationVariant>;
+
+/// The known kinds of sprite variation for representing different directions.
+#[derive(Debug, Reflect)]
+pub enum Directionality {
+    Zero, // Neutral
+    OneE, // E (animal, flip for W)
+    // OneN,  // N (spaceship, flip for S)
+    // TwoH,  // E, W
+    // Three, // E, N, S (flip for W)
+    Four, // E, N, W, S
+          // Five,  // E, NE, N, S, SE (flip for W, NW, SW)
+          // Eight, // 💪🏽💪🏽💪🏽
+}
+
+/// Data for an individual animation frame. This struct contains coordinates for
+/// some points and rectangles. The points have some particular frame of
+/// reference (described in comments), but the rectangles are all relative to
+/// the origin point and laid out in Bevy spatial coordinate space (y-up).
+#[derive(Debug)]
+pub struct CharAnimationFrame {
+    /// Index into the `TextureAtlas`.
+    pub index: usize,
+    /// Frame duration.
+    pub duration: Duration,
+    /// Origin point of the sprite in pixel coordinates, where 0,0 is top left.
+    /// Stored on the frame for convenience, but shouldn't vary.
+    pub origin: Vec2,
+    /// Anchor point that can be assigned to a TextureAtlasSprite. Same as
+    /// origin, but transformed to normalized coordinates relative to the
+    /// texture size (where 0,0 is the center and -.5,-.5 is bottom left).
+    pub anchor: Vec2,
+    /// Bbox for the projected foot position on the ground.
+    pub walkbox: Option<Rect>,
+    /// Bbox for the damage-dealing area of a frame.
+    pub hitbox: Option<Rect>,
+    // todo: hurtbox,
+}
+
+#[derive(Event)]
+pub struct AnimateFinishedEvent(pub Entity);
+
+#[derive(Component, Debug)]
+pub struct CharAnimationState {
+    pub animation: Handle<CharAnimation>,
+    pub variant: Option<VariantName>,
+    // Whether to flip the sprite. Set at same time as variant, bc it's tied
+    // to the directionality of the animation as a whole. Saved on state struct
+    // bc of where we need to use it (animate_system).
+    pub flip_x: bool,
+    pub playback: Playback,
+    pub frame: usize,
+    // To start with, we'll just always loop.
+    pub frame_timer: Option<CountupTimer>,
+    /// Optionally override the animation's frame timings. Can set all
+    /// frames to a uniform duration (in ms), split a given duration among all
+    /// frames, or scale all frames by some factor.
+    pub frame_time_override: FrameTimeOverride,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Playback {
+    Loop,
+    Once,
+}
+
+/// Allow programmatically overriding the frame times from the animation source
+/// data, for things like stretching out a motion to fit it to a particular
+/// total duration.
+#[derive(Debug, Clone, Copy)]
+pub enum FrameTimeOverride {
+    None,
+    Ms(u64),
+    Scale(f32),
+    TotalMs(u64),
+}
+
+impl CharAnimationState {
+    pub fn new(animation: Handle<CharAnimation>, variant: VariantName, playback: Playback) -> Self {
+        CharAnimationState {
+            animation,
+            variant: Some(variant),
+            flip_x: false,
+            playback,
+            // in the future I might end up wanting to blend between animations
+            // at a particular frame. Doesn't matter yet tho.
+            frame: 0,
+            frame_timer: None,
+            frame_time_override: FrameTimeOverride::None,
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.frame = 0;
+        self.frame_timer = None;
+        self.frame_time_override = FrameTimeOverride::None;
+    }
+
+    /// Change direction of animation, unless already facing the requested direction.
+    pub fn change_variant(&mut self, variant: VariantName) {
+        if self.variant != Some(variant) {
+            self.variant = Some(variant);
+            self.reset();
+        }
+    }
+
+    pub fn change_animation(&mut self, animation: Handle<CharAnimation>, playback: Playback) {
+        if self.animation != animation {
+            self.animation = animation;
+            // TODO: I'm leaving the variant the same, but actually I don't know if
+            // that's the right move -- question is whether I'd ever switch to an
+            // animation with fewer variants.
+            self.playback = playback;
+            self.reset();
+        }
+    }
+
+    pub fn _set_frame_times_to(&mut self, millis: u64) {
+        self.frame_time_override = FrameTimeOverride::Ms(millis);
+    }
+
+    pub fn _scale_frame_times_by(&mut self, scale: f32) {
+        self.frame_time_override = FrameTimeOverride::Scale(scale);
+    }
+
+    pub fn set_total_run_time_to(&mut self, millis: u64) {
+        self.frame_time_override = FrameTimeOverride::TotalMs(millis);
+    }
+
+    pub fn timer_just_finished(&self) -> bool {
+        matches!(
+            self.frame_timer.as_ref().map(|t| t.just_finished()),
+            Some(true)
+        )
+    }
+}
