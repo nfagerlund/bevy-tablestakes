7152936056e930a25c40d6471e33855c297d0e9c
Nick Fagerlund <nick.fagerlund@gmail.com>
2023-10-17 22:01:23 -0500

    comments: Clarify rect mirroring

--- src/toolbox.rs
+++ src/toolbox.rs
@@ -12,16 +12,18 @@ pub fn flip_vec2_x(v: Vec2) -> Vec2 {
     Vec2::new(-(v.x), v.y)
 }
 
-/// Mirror a Rect vertically. Note that mirroring each corner yields valid
-/// opposite corners in the new mirrored rectangle, but they're the top left
-/// and bottom right, rather than the bottom left (min) and top right (max).
-/// (Or, vice-versa if you're using a top-down Y coordinate. you know what I mean.)
-/// Anyway, Rect::from_corners can sort that out.
+/// Mirror a Rect vertically, around the implied origin point that the corners
+/// are defined relative to.
 pub fn flip_rect_y(r: Rect) -> Rect {
+    // Note that mirroring each corner yields valid opposite corners in the new
+    // mirrored rectangle, but they're the top left and bottom right, rather
+    // than the bottom left (min) and top right (max). (Or, vice-versa if
+    // you're using a top-down Y coordinate. you know what I mean.)
+    // Anyway, Rect::from_corners can sort that out.
     Rect::from_corners(flip_vec2_y(r.min), flip_vec2_y(r.max))
 }
 
-/// Mirror a Rect horizontally.
+/// Mirror a Rect horizontally around the implied origin.
 pub fn flip_rect_x(r: Rect) -> Rect {
     Rect::from_corners(flip_vec2_x(r.min), flip_vec2_x(r.max))
 }
